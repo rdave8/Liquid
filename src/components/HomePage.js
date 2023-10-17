0be8bd7feb2a7aa7a9015d5ae4b1ef29d345fc2d
rohanDave8 <rohan8dave@gmail.com>
2023-10-17 16:27:27 -0500

    Ripple

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import WaterWave from 'react-water-wave';
 import '../styles/HomePage.css';
+import background from '../resources/background.png'
 
 const HomePage = () => {
   return (
-    <>
-    <div class="img"></div>
-    <div class="center">
-        <div class="title">Boundless Collaboration</div>
-        <div class="subtitle">Safely tap into distributed data with fluid AI training pools</div>
-        <div class="btns">
-        <button>Join a Pool</button>
-        <button>Create a Pool</button>
-        </div>
-    </div>
-    </>
+    <WaterWave 
+    imageUrl={background} 
+    style={{ width: '100%', height: '100vh', backgroundSize: 'cover' }}
+    resolution={512}
+    dropRadius={50}
+    perturbance={0.01}
+    >
+      {() => (
+          <div className="center">
+            <div className="title">Boundless Collaboration</div>
+            <div className="subtitle">Safely tap into distributed data with fluid AI training pools</div>
+            <div className="btns">
+              <button>Join a Pool</button>
+              <button>Create a Pool</button>
+            </div>
+          </div>
+      )}
+    </WaterWave>
   );
 };
 
