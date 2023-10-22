93a5997a520888142d14584def52ddbfc2147127
rohanDave8 <rohan8dave@gmail.com>
2023-10-21 23:22:16 -0500

    fixed connect button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useWeb3Modal } from '@web3modal/wagmi/react';
+import { useAccount } from 'wagmi'
 import '../styles/Navbar.css';
 
 const Navbar = () => {
   const location = useLocation();
   const currentPath = location.pathname;
 
+  const { isConnected } = useAccount()
   const { open } = useWeb3Modal();
+
+  const [, setIsConnected] = useState(!!localStorage.getItem('isConnected'));
+
+  const connectWallet = () => {
+    open();
+    if (isConnected) {
+      setIsConnected(true);
+      localStorage.setItem('isConnected', 'true');
+    }
+  };
+
+  useEffect(() => {
+    const localIsConnected = localStorage.getItem('isConnected');
+    if (localIsConnected === 'true') {
+      setIsConnected(true);
+    }
+  }, []);
 
   return (
     <nav>
@@ -26,7 +45,9 @@ const Navbar = () => {
             <a href="/LearnMore" className={currentPath === '/LearnMore' ? 'active' : ''}>Learn More</a>
           </li>
           <li>
-            <a className="unconnected" href="#" onClick={() => open()}>Connect Wallet</a>
+            <a className={`unconnected ${isConnected ? 'connected' : ''}`} href="#" onClick={connectWallet}>
+              {isConnected ? 'Connected' : 'Connect Wallet'}
+            </a>
           </li>
         </ul>
       </div>
